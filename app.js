c6166c40b4a83d39b370a5274d430cf626a27f5f
stefanbolba <stefanbolba@gmail.com>
2020-08-03 03:18:34 +0100

    Fixed bugs

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const morgan = require('morgan');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const cookieParser = require('cookie-parser');
-const compression = require('compression');
+// const compression = require('compression');
 
 const issueRouter = require('./routes/issueRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -42,8 +42,9 @@ app.use(mongoSanitize());
 // Data sanitization against XSS
 app.use(xss());
 
+//Creates bugs. Need to investigate more
 //Compression midleware
-app.use(compression);
+// app.use(compression);
 
 //ROUTES
 app.use('/', viewRouter);
